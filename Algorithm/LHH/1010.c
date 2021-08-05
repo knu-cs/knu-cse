bf9c241f471af7c2fdb30ad8c5848c9135db1e2b
hoyeon <ih2116501@gmail.com>
2021-08-05 21:52:04 +0900

    for PR test

---
+++ Algorithm/LHH/1010.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int Combination(int n, int r);
+int main()
+{
+    int num_right_site, num_left_site;
+    int num_test_case;
+    int i;
+    scanf("%d", &num_test_case);
+
+    for(i = 0; i<num_test_case; i++){
+        scanf("%d %d", &num_left_site, &num_right_site);
+        printf("%d\n", Combination(num_right_site, num_left_site));
+    }
+}
+
+
+int Combination(int n, int r)
+{
+    if(r==0)
+        return 1;
+    else if(n == r)
+        return 1;
+    else
+        return Combination(n-1, r-1)+Combination(n-1, r);
+}
---
+++ Algorithm/LHH/1015.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#define MAX_NUM 50
+
+int ftemp(int arr[], int size_of_array);
+
+int main()
+{
+    int size_of_array;
+    int A[MAX_NUM];
+    int i = 0;
+    scanf("%d", &size_of_array);
+    
+    for(i = 0; i< size_of_array; i++){
+        scanf("%d", &A[i]);
+    }
+
+    ftemp(A, size_of_array);
+    return 0;
+}
+
+int ftemp(int arr[], int size_of_array){
+    int P[MAX_NUM] = {0,};
+    int i, j;
+    for(i=0; i<size_of_array; i++){
+        for(j = 0; j<size_of_array; j++){
+            if(i == j)
+                continue;
+            if(arr[i] > arr[j])
+                P[i]++;
+            if(arr[i] == arr[j] && i>j)
+                P[i]++;
+        }
+    }
+    
+    for(i=0; i<size_of_array; i++){
+        printf("%d", P[i]);
+    }
+}
